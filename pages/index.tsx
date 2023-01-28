7e4a0490abb210d4dff6f34b067cd46b4f1b1688
LeeSeungjae00 <seungjae266789@gmail.com>
2023-01-29 01:12:36 +0900

    :sparkles: Intrduce new features
    데이터 베이스에 created by 추가 및 화면 표출

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   // const [products, setProducts] = useState<{ id: string; properties: { id: string }[] }[]>([]);
-  const [products, setProducts] = useState<{ id: string; name: string }[]>([]);
+  const [products, setProducts] = useState<{ id: string; name: string; createdAt: string }[]>([]);
   const inputRef = useRef<HTMLInputElement>(null);
   // useEffect(() => {
   //   fetch(`/api/get-items`)
@@ -44,7 +44,12 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <input type="text" ref={inputRef} placeholder="name" />
+          <input
+            className="placeholder:italic placeholder:text-slate-400 block bg-white w-100 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            type="text"
+            ref={inputRef}
+            placeholder="name"
+          />
           <button onClick={handleClick}>Add Jacket</button>
           <div>
             {/* <p>Product List</p>
@@ -70,7 +75,13 @@ export default function Home() {
                 </div>
               ))} */}
 
-            {products && products.map(item => <div key={item.id}>{item.name}</div>)}
+            {products &&
+              products.map(item => (
+                <div key={item.id}>
+                  {item.name}
+                  <span>{item.createdAt}</span>
+                </div>
+              ))}
           </div>
         </div>
       </main>
