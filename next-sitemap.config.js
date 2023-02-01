b7887e176b833001865259f7d57cdd536c676fe5
LeeSeungjae00 <seungjae266789@gmail.com>
2023-02-01 17:15:22 +0900

    :mag: Imporve SEO.
    next-sitemap 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://nextjs-tutorial-2rseatng3-leeseungjae00.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+  // sourceDir: 'build'
+};
