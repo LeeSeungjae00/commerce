ae1f33077065327401b3a9abd16b78bda4ba4fb6
LeeSeungjae00 <seungjae266789@gmail.com>
2023-02-05 14:24:59 +0900

    :sparkles: Introduce new features.
    상품 목록들 list 가져오는 api 및 화면 구현, pagenation 까지 등록

--- pages/api/get-products-count.ts
+++ pages/api/get-products-count.ts
@@ -4,12 +4,9 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function getProducts(skip: number, take: number) {
+async function getProductsCount() {
   try {
-    const response = prisma.products.findMany({
-      skip,
-      take,
-    });
+    const response = prisma.products.count();
     return response;
   } catch (e) {
     console.error(JSON.stringify(e));
@@ -23,12 +20,7 @@ type res = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<res>) {
   try {
-    const { skip, take } = req.query;
-    if (skip == null || take == null) {
-      res.status(400).json({ message: `no skip or take` });
-      return;
-    }
-    const products = await getProducts(Number(skip), Number(take));
+    const products = await getProductsCount();
     res.status(200).json({ items: products, message: `Success` });
   } catch (error) {
     return res.status(400).json({ message: 'Fail' });
---
+++ pages/products/index.tsx
@@ -0,0 +1,56 @@
+import { useCallback, useEffect, useState } from 'react';
+import { products } from '@prisma/client';
+import Image from 'next/image';
+
+const TAKE = 9;
+export default function Products() {
+  const [skip, setSkip] = useState(0);
+  const [products, setProducts] = useState<products[]>([]);
+
+  useEffect(() => {
+    fetch(`/api/get-products?skip=0&take=${TAKE}`)
+      .then(res => res.json())
+      .then(res => setProducts(res.items));
+    return () => {};
+  }, []);
+
+  const getProducts = useCallback(() => {
+    const next = skip + TAKE;
+    fetch(`/api/get-products?skip=${next}&take=${TAKE}`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        setProducts(products => [...products, ...res.items]);
+      });
+    setSkip(next);
+  }, [skip]);
+
+  return (
+    <div className="px-36 mt-36 mb-36">
+      <div className="grid grid-cols-3 gap-5">
+        {products &&
+          products.map(item => (
+            <div key={item.id}>
+              <Image
+                className="rounded"
+                alt={item.name}
+                src={item.image_url ?? ''}
+                width={300}
+                height={200}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP0dPesBwADFAFamsrLhQAAAABJRU5ErkJggg=="
+              ></Image>
+              <div className="flex">
+                <span>{item.name}</span>
+                <span className="ml-auto">{item.price.toLocaleString('ko-KR')}원</span>
+              </div>
+              <span className="text-zinc-400">{item.category_id === 1 && '의류'}</span>
+            </div>
+          ))}
+      </div>
+      <button className="w-full rounded mt-20 bg-zinc-200 p-4" onClick={getProducts}>
+        더보기
+      </button>
+    </div>
+  );
+}
