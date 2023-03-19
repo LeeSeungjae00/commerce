983f4b7fef7c0f34c4fa9621dc978707e90c55d6
LeeSeungjae00 <seungjae266789@gmail.com>
2023-03-19 23:51:34 +0900

    :sparkles: Introduce new features.
    상품의 후기들을 가져오는 api 추가

---
+++ pages/api/get-comments.ts
@@ -0,0 +1,53 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { OrderItem, PrismaClient } from '@prisma/client';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from './auth/[...nextauth]';
+
+const prisma = new PrismaClient();
+
+async function getComments(productId: number) {
+  try {
+    const orderItems = await prisma.orderItem.findMany({
+      where: {
+        productId,
+      },
+    });
+
+    console.log(orderItems);
+
+    let response = [];
+
+    for (const orderItem of orderItems) {
+      const res = await prisma.comment.findUnique({
+        where: {
+          orderItemId: orderItem.id,
+        },
+      });
+      response.push({
+        ...orderItem,
+        ...res,
+      });
+    }
+
+    return response;
+  } catch (e) {
+    console.error(JSON.stringify(e));
+  }
+}
+
+type res = {
+  items?: any;
+  message: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<res>) {
+  const { productId } = req.query;
+
+  try {
+    const wishlist = await getComments(Number(productId));
+    res.status(200).json({ items: wishlist, message: `Success` });
+  } catch (error) {
+    return res.status(400).json({ message: 'Fail' });
+  }
+}
